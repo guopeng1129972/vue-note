7d8c20874ac70c616d2b2c75993f5b9b27fa4975
guopeng1129972 <381694179@qq.com>
2020-08-23 01:05:06 +0800

    vuex 状态管理

--- dome/5.vue-cli/vue-ui/README.md
+++ dome/5.vue-cli/vue-ui/README.md
@@ -1,24 +1,11 @@
-# vue-ui
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+commit 1
+vuex 状态管理
+view->Actions->State
+如何使用vuex?
+1.定义Store文件
+2.引用vuex
+Vue.use(Vuex)
+3.定义
+state 就是状态
+mutations 就是改变状态的函数 方法
+store.commit('aaa') 就可以理解为给store说这块有一个状态需要改变
--- dome/5.vue-cli/vue-ui/src/views/Info.vue
+++ dome/5.vue-cli/vue-ui/src/views/Info.vue
@@ -1,17 +1,27 @@
 <template>
 <div class="home">
   <p>this is page info</p>
+  <button type="button" @click="add()">点击添加</button>
+  <p>{{msg}}</p>
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import Hello from '@/components/Hello.vue';
+import store from '@/store'
 
-// export default {
-//   name: 'Hello',
-//   components: {
-//     Hello,
-//   },
-// };
+export default {
+  name:'Info',
+  data(){
+    return{
+    msg:store.state.count
+  }},
+  methods:{
+    add(){
+      console.log('add Event from info!')
+      store.commit('addCount')
+    }
+  }
+};
 </script>
