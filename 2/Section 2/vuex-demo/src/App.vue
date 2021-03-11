65d572a9ce6929883d9b12d072622ff22bc9cbf8
guopeng1129972 <381694179@qq.com>
2021-03-11 15:06:51 +0800

    19 | 如何在Vue中使用Vuex

---
+++ 2/Section 2/vuex-demo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="app">
+  {{count}}
+  <br />
+  {{count2}}
+  <button @click="countAdd">count++</button>
+  <button @click="countAdd2">count++</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    count() {
+      return this.$store.state.count
+    },
+    count2() {
+      return this.$store.getters.doubleCount
+    }
+
+  },
+  methods: {
+    countAdd() {
+      // commit increment 到 Vuex.Store.mutations 
+      return this.$store.commit('increment')
+    },
+    countAdd2() {
+      // dispatch increment 到 Vuex.Store.actions 
+      return this.$store.dispatch('increment')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
