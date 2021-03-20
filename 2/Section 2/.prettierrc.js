f80bfc723363f5221ae9dd9367108da0f5b8fd9b
guopeng1129972 <381694179@qq.com>
2021-03-20 20:52:49 +0800

    27 | 提升开发效率和体验的常用工具：ESLint、Prettier、vue-devtools

---
+++ 2/Section 2/.prettierrc.js
@@ -0,0 +1,4 @@
+module.exports={
+  singleQuote:true, //只用单引号
+  trailingComma:'all',//命名对象都加逗号 ,
+}
---
+++ 2/Section 2/test.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+const a = '123';
+export default {
+  props: {
+    message: {
+      type: Number,
+      default: '0',
+      a: "b",
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
