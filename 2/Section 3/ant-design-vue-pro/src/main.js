c8e58ca68c6198d0412df1c521d89bb4f4c0698c
guopeng1129972 <381694179@qq.com>
2021-03-23 22:14:07 +0800

    32 | 使用Vue CLI 3快速创建项目

---
+++ 2/Section 3/ant-design-vue-pro/src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Antd from 'ant-design-vue'
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import 'ant-design-vue/dist/antd.less'
+
+Vue.config.productionTip = false;
+// 全局注册 Antd
+Vue.use(Antd);
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
