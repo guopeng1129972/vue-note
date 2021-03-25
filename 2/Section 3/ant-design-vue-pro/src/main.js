4245873b46cce3cb89e25604717a1b974fa3512b
guopeng1129972 <381694179@qq.com>
2021-03-25 14:59:56 +0800

    34 | 如何设计一个高扩展性的路由

--- 2/Section 3/ant-design-vue-pro/src/main.js
+++ 2/Section 3/ant-design-vue-pro/src/main.js
@@ -1,16 +1,16 @@
 import Vue from "vue";
-// import Antd from "ant-design-vue"
-import Button from "ant-design-vue/lib/button"
+import Antd from "ant-design-vue"
+// import Button from "ant-design-vue/lib/button"
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// import 'ant-design-vue/dist/antd.less'
-import "ant-design-vue/lib/button/style"
+import 'ant-design-vue/dist/antd.less'
+// import "ant-design-vue/lib/button/style"
 
 Vue.config.productionTip = false;
 // 全局注册 Antd
-// Vue.use(Antd);
-Vue.use(Button);
+Vue.use(Antd);
+// Vue.use(Button);
 
 new Vue({
   router,
--- 2/Section 3/ant-design-vue-pro/src/router/index.js
+++ 2/Section 3/ant-design-vue-pro/src/router/index.js
@@ -1,23 +1,106 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Nprogress from "nprogress";
+import "nprogress/nprogress.css"
+import NotFound from "../views/404.vue";
+
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/user',
+    component: () => import(/* webpackChunkName: "layout" */  "../layouts/UserLayout.vue"),
+    children: [
+      {
+        path: '/user',
+        redirect: '/user/login'
+      },
+      {
+        path: '/user/login',
+        name: "login",
+        component: () =>
+          import(/* webpackChunkName: "user" */  "../views/User/Login.vue")
+      },
+      {
+        path: '/user/register',
+        name: "register",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../views/User/Register.vue"),
+      }
+    ]
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/',
+    component: () => import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout.vue"),
+    children: [
+      //dashboard
+      {
+        path: "/",
+        redirect: '/dashboard/analysis'
+      },
+      {
+        path: '/dashboard',
+        name: "dashboard",
+        component: { render: h => h("router-view") },
+        children: [
+          {
+            path: "/dashboard/analysis",
+            name: "analysis",
+            component: () => import(/* webpackChunkName: "Dashboard" */ "../views/Dashboard/Analysis.vue"),
+          }
+        ]
+      },
+      // form
+      {
+        path: '/form',
+        name: "form",
+        component: { render: h => h("router-view") },
+        children: [
+          {
+            path: '/form',
+            redirect: '/form/basic-form',
+          },
+          {
+            path: "/form/basic-form",
+            name: "basicform",
+            component: () => import(/* webpackChunkName: "Froms" */ "../views/Froms/BasicForm.vue"),
+          },
+          {
+            path: "/form/step-form",
+            name: "stepform",
+            component: () => import(/* webpackChunkName: "Froms" */ "../views/Froms/StepForm"),
+            children: [
+              {
+                path: "/form/step-form",
+                redirect: "/form/step-form/info"
+              },
+              {
+                path: "/form/step-form/info",
+                name: "info",
+                component: () => import(/* webpackChunkName: "Froms" */ "../views/Froms/StepForm/Step1.vue"),
+              },
+              {
+                path: "/form/step-form/confirm",
+                name: "confirm",
+                component: () => import(/* webpackChunkName: "Froms" */ "../views/Froms/StepForm/Step2.vue"),
+              },
+              {
+                path: "/form/step-form/result",
+                name: "result",
+                component: () => import(/* webpackChunkName: "Froms" */ "../views/Froms/StepForm/Step3.vue"),
+              }
+            ]
+          }
+        ]
+      },
+    ]
+  },
+  {
+    path: "*",
+    name: "404",
+    component: NotFound,
   },
 ];
 
@@ -26,5 +109,12 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+router.beforeEach((to, from, next) => {
+  Nprogress.start()
+  next()
+});
+router.afterEach(() => {
+  Nprogress.done()
+})
 
 export default router;
